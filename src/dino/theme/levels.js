65344fbd5c2ff6804666319210c46aa7c650b1d6
diegoperg-prog <diego.perg@gmail.com>
2025-10-26 01:58:28 -0300

    Update levels.js

--- src/dino/theme/levels.js
+++ src/dino/theme/levels.js
@@ -1,1 +1,46 @@
+const levels = [
+  {
+    id: 1,
+    name: "Bosque Místico",
+    from: "#3E7C59",
+    to: "#F2F2F7",
+    text: "#3E7C59",
+  },
+  {
+    id: 2,
+    name: "Aldea al Amanecer",
+    from: "#E6B450",
+    to: "#FFF5E1",
+    text: "#B86E00",
+  },
+  {
+    id: 3,
+    name: "Montañas / Ruinas",
+    from: "#5A6B8C",
+    to: "#E6EBF4",
+    text: "#3C4A64",
+  },
+  {
+    id: 4,
+    name: "Cueva Oscura / Abismo",
+    from: "#4C3A6E",
+    to: "#E8E4F2",
+    text: "#2F2451",
+  },
+  {
+    id: 5,
+    name: "Cima / Castillo",
+    from: "#3A6FE6",
+    to: "#DDE8FF",
+    text: "#2C4CA8",
+  },
+  {
+    id: 6,
+    name: "Santuario Luminoso",
+    from: "#F6D673",
+    to: "#FFF9E6",
+    text: "#A27B00",
+  },
+];
+export default levels;
 
